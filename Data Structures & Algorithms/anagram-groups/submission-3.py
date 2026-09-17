22635941986c7e80293825659f649d9fcbb4127e
Kawsar Kamal <kawsarkamal@gmail.com>
2026-09-17 08:54:30 -0400

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+  
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+
+        for i in range(len(strs)): #act, pots
+            key_list = sorted(strs[i])
+            key = ""
+            for j in key_list:
+                key = key + j
+
+            if key in anagrams:
+                anagrams[key].append(strs[i])
+            else:
+                anagrams[key] = [strs[i]]
+        
+        return list(anagrams.values())
+
+    
+    
+    def isAnagram(self, a: list, b: list):
+        if len(a) != len(b):
+            return False
+
+        comp = [0]*26
+
+        for i in range(len(a)):
+            comp[ord(a[i])-ord('a')] += 1
+            comp[ord(b[i])-ord('a')] -= 1
+        
+        for i in comp:
+            if i != 0:
+                return False
+
+        return True
+
+        
+
+        
